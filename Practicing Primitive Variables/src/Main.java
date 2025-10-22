4548e23e3c557da2d64578ccf8dbd15aaa5d9402
Henrique Morey Pereira <henriquemoreypereira@gmail.com>
2025-10-22 07:45:56 -0300

    feat: Adiciona média usando double

--- Practicing Primitive Variables/src/Main.java
+++ Practicing Primitive Variables/src/Main.java
@@ -2,6 +2,8 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        
+        double valorA=2, valorB = 3, media=(valorA+valorB)/2;
+
+        System.out.println("A media de A e B é: "+ media);
     }
 }
