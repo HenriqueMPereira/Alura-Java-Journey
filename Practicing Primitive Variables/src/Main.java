c3ab4f7e5cbdbd180ffbf75b80638d1282672405
Henrique Morey Pereira <henriquemoreypereira@gmail.com>
2025-10-22 08:04:36 -0300

    feat: Adiciona o exercício de variáveis primitivas por completo

--- Practicing Primitive Variables/src/Main.java
+++ Practicing Primitive Variables/src/Main.java
@@ -5,5 +5,24 @@ public class Main {
         double valorA=2, valorB = 3, media=(valorA+valorB)/2;
 
         System.out.println("A media de A e B é: "+ media);
+
+        int castDoubleToInt = (int)media;
+        System.out.println("A média era " + media + " e ficou "+ castDoubleToInt + " após o casting");
+
+        char c = 'H'; // Para char devemos usar aspas simples
+        String name = "A inicial do nome Henrique é ";
+        String phrase = name + c;
+        System.out.println(phrase);
+
+        double priceOfCandy = 1.69;
+        int numberOfCandy = 100;
+        System.out.println("O valor total dos doces é: " + priceOfCandy*numberOfCandy);
+
+        double valorEmDolares = 11, valorEmReais = 4.94 * valorEmDolares;
+        System.out.println("O valor em reais é: " + valorEmReais);
+
+        double precoOriginal = 8, percentualDesconto = 10, valorDescontado;
+        valorDescontado = (precoOriginal*(100-percentualDesconto))/100;
+        System.out.println("O valor final já com o desconto é: " + valorDescontado);
     }
 }
