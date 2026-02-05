b3af532c907c4fd4fd190f69f72d70c130883384
Henrique Morey Pereira <henriquemoreypereira@gmail.com>
2026-02-05 16:28:35 -0300

    feat: Adiciona na classe Menu: 1) Busca de episódio usando Stream e Optional, 2) Cálculo da média das avaliações por temporada usando Map, Stream (averagingDouble) e 3) Cálculo das estatísticas usando DoubleSummaryStatistics e Stream (summarizingDouble).

--- SpringScreenMatch/screenmatch/src/main/java/io/github/henriquempereira/screenmatch/view/Menu.java
+++ SpringScreenMatch/screenmatch/src/main/java/io/github/henriquempereira/screenmatch/view/Menu.java
@@ -9,10 +9,7 @@ import io.github.henriquempereira.screenmatch.services.DataConverter;
 
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Menu {
@@ -27,37 +24,46 @@ public class Menu {
     private final String API_KEY = "&apikey=e8be24d1";
 
 
-    public void starMenu(){
+    public void starMenu() {
         System.out.println("Digite um nome de serie:");
         String serieName = scanner.nextLine();
 
+        /*
+         * Fazendo requisição dos dados da série
+         */
         String json = apiClient.fetchData(ADRESS + URLEncoder.encode(serieName, StandardCharsets.UTF_8) + API_KEY);
         SeriesData seriesData = dataConverter.convertTo(json, SeriesData.class);
-        System.out.println("\n\nDados da série:\n" + seriesData + "\n\n");
 
+        /*
+         * Fazendo requisição e guardando todas as temporadas em uma lista
+         */
         List<SeasonData> seasonDataList = new ArrayList<>();
-        for(int i = 1; i <= seriesData.numberOfSeasons(); i++){
-            json = apiClient.fetchData(ADRESS+ URLEncoder.encode(serieName, StandardCharsets.UTF_8) + "&season=" + i + API_KEY);
+        for (int i = 1; i <= seriesData.numberOfSeasons(); i++) {
+            json = apiClient.fetchData(ADRESS + URLEncoder.encode(serieName, StandardCharsets.UTF_8) + "&season=" + i + API_KEY);
             SeasonData seasonData = dataConverter.convertTo(json, SeasonData.class);
             seasonDataList.add(seasonData);
         }
-        seasonDataList.forEach(System.out::println);
-        //seasonDataList.forEach(t -> t.episodes().forEach(e -> System.out.println(e.title())));
 
-        System.out.println("\n\n***************************************************************");
-
+        /*
+         * Pegando a lista de temporadas e transformando em uma lista de episodios
+         * "pega as caixas com bolas, tira as bolas e coloca dentro de UM saco"
+         */
         List<EpisodeData> episodesList = seasonDataList.stream()
-                        .flatMap(s -> s.episodes().stream())
-                                .collect(Collectors.toList());
+                .flatMap(s -> s.episodes().stream())
+                .collect(Collectors.toList());
 
-        //System.out.println(episodesList);
-
+        /*
+         * Printa os 5 piores episódios
+         */
         episodesList.stream()
                 .filter(e -> !e.rating().equalsIgnoreCase("N/A"))
-                .sorted(Comparator.comparing(EpisodeData::rating).reversed())
+                .sorted(Comparator.comparing(EpisodeData::rating))
                 .limit(5)
                 .forEach(System.out::println);
 
+        /*
+         * Cria uma lista da classe Episode
+         */
         List<Episode> episodes = seasonDataList.stream()
                 .flatMap(s -> s.episodes().stream()
                         .map(e -> new Episode(s.season(), e)))
@@ -65,5 +71,40 @@ public class Menu {
 
         System.out.println(episodes);
 
+        /*
+         * Verificando se um episódio está na lista de Episódios.
+         * - usando Stream e Optional
+         */
+        System.out.println("\nDigite nome (ou pedaço de nome) do episódio que deseja pesquisar: ");
+        String episodeTitle = scanner.nextLine();
+
+        Optional<Episode> searchedEpisode = episodes.stream()
+                .filter(e -> e.getTitle().toUpperCase().contains(episodeTitle.toUpperCase()))
+                .findFirst();
+
+        if (searchedEpisode.isPresent()) {
+            System.out.println("O episódio foi encontrado!\nTemporada: " + searchedEpisode.get().getSeason());
+        } else {
+            System.out.println("Episódio não encontrado...");
+        }
+
+        /*
+         * Fazendo a média da nota das avaliações por temporada
+         */
+        Map<Integer, Double> ratingsPerSeason = episodes.stream()
+                .filter(e -> e.getRating() != 0)
+                .collect(Collectors.groupingBy(Episode::getSeason ,
+                        Collectors.averagingDouble(Episode::getRating)));
+        System.out.println("Avaliação por temporada: " + ratingsPerSeason);
+
+        /*
+         * Calculando estatísticas sobre avaliações (Rating) dos episódios
+         * É aplicado um filtro para que não pegue nenhum episódio que não foi avaliado
+         */
+        DoubleSummaryStatistics est = episodes.stream()
+                .filter(e -> e.getRating() != 0)
+                .collect(Collectors.summarizingDouble(Episode::getRating));
+
+        System.out.println(est);
     }
 }
